6d57d0dec5f6264bb83c82b77021831417db8b1b
Luka Butina <luka.butina@gmail.com>
2019-08-12 10:59:57 +0200

    Add responsvive styling and mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 var headerEl = document.querySelector(".region-header");
 
+headerEl.innerHTML +=
+  '<button class="mobile-menu"><img src="/themes/adchallenge/images/hamburger.svg"></button>';
+
+var mobileMenuBtEl = document.querySelector(".mobile-menu");
+var mobileMenuEl = document.querySelector("header nav");
+
 function onScrollEventHandler(ev) {
   var docViewTop = window.pageYOffset;
   if (docViewTop > 10) {
@@ -9,6 +15,18 @@ function onScrollEventHandler(ev) {
   }
 }
 
+function mobileMenu(ev) {
+  var mobileMenuImg = document.querySelector(".mobile-menu img");
+  if (mobileMenuEl.classList.contains("open")) {
+    mobileMenuImg.src = "/themes/adchallenge/images/hamburger.svg";
+  } else {
+    mobileMenuImg.src = "/themes/adchallenge/images/cross.svg";
+  }
+  mobileMenuEl.classList.toggle("open");
+}
+
+if (mobileMenuBtEl.addEventListener)
+  mobileMenuBtEl.addEventListener("click", mobileMenu, false);
 if (window.addEventListener)
   window.addEventListener("scroll", onScrollEventHandler, false);
 else if (window.attachEvent)
